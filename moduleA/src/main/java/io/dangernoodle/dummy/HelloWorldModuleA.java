c09d85d7e13c752f84199b1b4d7ac218244b566e
Jae Gangemi <jgangemi@gmail.com>
2016-09-20 11:24:51 -0400

    - initial check-in

---
+++ moduleA/src/main/java/io/dangernoodle/dummy/HelloWorldModuleA.java
@@ -0,0 +1,12 @@
+package io.dangernoodle.dummy;
+
+/**
+ * javadoc for <code>HelloWorldModuleA</code>
+ */
+public class HelloWorldModuleA
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
