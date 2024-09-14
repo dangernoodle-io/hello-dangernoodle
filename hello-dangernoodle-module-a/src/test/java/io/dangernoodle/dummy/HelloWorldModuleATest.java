09e1093b60abf7b6e700589dd878ade218687622
Jae Gangemi <jgangemi@gmail.com>
2024-09-13 19:11:31 -0600

    - coveralls testing

---
+++ hello-dangernoodle-module-a/src/test/java/io/dangernoodle/dummy/HelloWorldModuleATest.java
@@ -0,0 +1,24 @@
+package io.dangernoodle.dummy;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldModuleATest
+{
+    private HelloWorldModuleA moduleA;
+
+    @BeforeEach
+    public void setup()
+    {
+        moduleA = new HelloWorldModuleA();
+    }
+
+    @Test
+    public void testFoo()
+    {
+        assertTrue(moduleA.isFoo());
+    }
+
+}
