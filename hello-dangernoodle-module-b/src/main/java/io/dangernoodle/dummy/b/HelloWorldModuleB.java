7a813dd0f7ccd2500ac9ad216e9050aeb634bf0a
Jae Gangemi <jgangemi@gmail.com>
2024-10-02 13:49:35 -0600

    - moar coveralls (#12)
    - refactoring for javadoc generation testing

--- hello-dangernoodle-module-b/src/main/java/io/dangernoodle/dummy/b/HelloWorldModuleB.java
+++ hello-dangernoodle-module-b/src/main/java/io/dangernoodle/dummy/b/HelloWorldModuleB.java
@@ -1,10 +1,15 @@
-package io.dangernoodle.dummy;
+package io.dangernoodle.dummy.b;
 
 /**
  * javadoc for <code>HelloWorldModuleA</code>
  */
 public class HelloWorldModuleB
 {
+    public boolean isBaz()
+    {
+        return true;
+    }
+
     public static void main(String[] args)
     {
         System.out.println("hello world");
